15fe84cfa0dff8662b38d89ed4105e8c70f0f78a
hannan-khan-intelops <hkhan@intelops.dev>
2022-10-03 15:27:39 -0500

    added working docker_grpc_example

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # gRPC_example
-This tutorial covers how to create a simple Python application that uses the gRPC Protocol.
+This tutorial covers how to create a simple Python application that uses the gRPC Protocol.  
+It's recommended that you start with **protoc_test_drive**, to get a hang of gRPC, and the code that it outputs.  
+After, go through the **toy_example** to get an idea of how to make a simple client-server app using gRPC.  
+Lastly, you can go through the **docker_grpc_example**. In this example, you will be running the server and client in
+different containers, similar to what microservices are like. The client will use gRPC to communicate with the 
---
+++ docker_grpc_example/client/client.py
@@ -0,0 +1,31 @@
+from flask import Flask
+
+import grpc
+import fizzbuzz_pb2
+import fizzbuzz_pb2_grpc
+
+app = Flask('docker_grpc_example_client')
+
+
+# easy function to just run the client and wait for a response.
+@app.route('/')
+def run_client():
+    # create an insecure channel at port 8888
+    try:
+        # create that channel on your local host.
+        # the ip address here should match the IP that your server has printed out.
+        # Ex. 'Server located at: 172.17.0.3'
+        with grpc.insecure_channel('172.17.0.3:8888') as channel:
+            # we create a stub that uses that channel.
+            stub = fizzbuzz_pb2_grpc.ComputatorStub(channel)
+            # we send a request and get a response from the stub.
+            response = stub.ComputeFizzBuzz(fizzbuzz_pb2.FizzBuzzDFRequest(col_name="toy_example"))
+        # we print the response as a table in html. Thus, we have to replace '/n' with '<br>'
+        print("Client received the following response:")
+        return bytes(str(response), "utf-8").decode("unicode_escape").replace('\n', '<br>')
+    except Exception as e:
+        return "ERROR\n" + str(e)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
